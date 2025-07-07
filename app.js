2182f51b9184726baa1c75afe5197e1b4bec5901
GenioZx <eli.rodri14@email.com>
2025-07-07 15:16:28 -0400

    FIrst commit

---
+++ app.js
@@ -0,0 +1,48 @@
+// Snake
+
+
+// What do you need to creat the game?
+// Grid background, food to expand snake length, data structure for snake, score tracking. 
+
+//What i need to render it ?
+
+// As a user i want to see the landing page when i arrive to the website to know im in the right place
+// As a user i want to be able to be able to see a start button at the landing page.
+// As a user i want to be able to control the snake with keyboard arrow keys.
+// As a user i want to see the grid of gameplay.
+// As a user i want to see score 
+// As a user i want to see food
+// As a user i want to see snake expand after each food earned 
+
+
+
+
+
+
+
+// Define score - each snake movement will incease points by 2, score will increase once by 10 each time snake 
+// eats food
+
+// Define snake Head - initial snake position, place snake at the center of the screen to start off
+
+// Define snake Tail - tail will move farther away from head by one sqaure each time snake eats snake
+
+// Define snake lenght and growth - square will extend snake length in between head and tail after the 1st time snake 
+// eats food, and each time snake eats food thereafter
+
+// Define snake movement based on players choices (x and y axis movements) - Snake body will move according to snakes 
+// head based on players movement commands
+
+// Define snake speed - every time score increases 200, speed will increase by .25%, snake will start moving one square 
+// per second
+
+// Define keystrokes - players movements - keyboard keys used will be the directional keys on board 
+//cursorUp, cursorRight, cursorDown, cursorLeft
+
+//Define game win - player wins game once score reaches 25000 points
+
+// Define game over - if snake runs into itself
+
+// Define food position - food will start at a random location, food will appear in random location 
+// on grid after each time snake eats previous food
+
