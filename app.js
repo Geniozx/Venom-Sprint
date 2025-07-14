a8231e9c81cc1bee088c6c2d0c0501b191068924
GenioZx <eli.rodri14@email.com>
2025-07-13 21:34:01 -0400

    commit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 //What i need to render it ?
 
-// As a user i want to see the landing page when i arrive to the website to know im in the right place
 // As a user i want to be able to be able to see a start button at the landing page.
 // As a user i want to be able to control the snake with keyboard arrow keys.
 // As a user i want to see the grid of gameplay.
@@ -18,23 +17,20 @@
 
 // Define snake Head - initial snake position, place snake at the center of the screen to start off
 
-// Define snake Tail - tail will move farther away from head by one sqaure each time snake eats snake
-
 // Define snake lenght and growth - square will extend snake length in between head and tail after the 1st time snake
 // eats food, and each time snake eats food thereafter
 
 // Define snake movement based on players choices (x and y axis movements) - Snake body will move according to snakes
 // head based on players movement commands
 
-// Define snake speed - every time score increases 200, speed will increase by .25%, snake will start moving one square
-// per second
+// Define snake speed - every time ssnake eats food, speed will increase
 
 // Define keystrokes - players movements - keyboard keys used will be the directional keys on board
-//cursorUp, cursorRight, cursorDown, cursorLeft
+//cursorUp, cursorRight, cursorDown, cursorLeft?
 
 //Define game win - player wins game once score reaches 25000 points
 
-// Define game over - if snake runs into itself
+// Define game over - if snake runs into itself or goes to the borders
 
 // Define food position - food will start at a random location, food will appear in random location
 // on grid after each time snake eats previous food
